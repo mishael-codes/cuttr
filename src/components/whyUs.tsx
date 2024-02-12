bd3f87bca6423f3270b695aba5cbdd91aefefeb8
mishael-codes <enyimishael22@gmail.com>
2024-02-12 11:01:03 +0100

    minor changes

--- src/components/whyUs.tsx
+++ src/components/whyUs.tsx
@@ -9,7 +9,7 @@ const WhyUs = () => {
       desc: "Cuttr links are snappy and shareable, making them easy to share on social media platforms.",
     },
     {
-      icon: <Icon.FastForward size="50px" />,
+      icon: <Icon.Zap size="50px" />,
       title: "Fast",
       desc: "Cuttr links are concise and load faster than long URLs, improving user experience.",
     },
@@ -36,7 +36,7 @@ const WhyUs = () => {
       <h3 className="text-text font-semibold text-2xl relative z-30 after:content-[''] after:absolute after:w-1/4 after:h-[3px] after:bg-accent after:-z-10 after:left-11 after:top-8 after:rounded-lg">
         Why Cuttr
       </h3>
-      <div className="w-fit mt-14 grid place-items-center justify-center gap-5 md:gap-7 md:grid-cols-2 lg:grid-cols-3">
+      <div className="w-fit mt-14 grid place-items-center justify-center gap-5 md:gap-7 md:grid-cols-2 lg:grid-cols-3 shadow-[5px_5px_20px_rgba(25, 5, 44,1)]">
         {reasons.map((reason) => (
           <div
             key={reason.title}
@@ -49,7 +49,7 @@ const WhyUs = () => {
         ))}
       </div>
       <a href="">
-        <button className="flex items-center justify-center mt-10 rounded-lg w-80 bg-accent font-bold text-background p-3 border border-accent hover:bg-transparent hover:text-accent transition-all">
+        <button className="flex items-center justify-center mt-10 rounded-lg w-80 bg-shadow font-bold text-accent p-3 border border-accent hover:bg-transparent hover:translate-x-[20px] hover:text-accent transition-all">
           Get Started
           <Icon.ChevronsRight className="ml-2" />
         </button>
