5633a00059b0573d633d25839e2bc5d5210c056e
mishael-codes <enyimishael22@gmail.com>
2024-02-16 12:17:49 +0100

    hope vercel.json file redirects to notfound.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="signin" element={<SignIn />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
