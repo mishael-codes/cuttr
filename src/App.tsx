7118631ef256fab20f5cb539a53cc954e0247e9b
mishael-codes <enyimishael22@gmail.com>
2024-02-16 08:37:15 +0100

    added a 404 page and a redirect for vercel added a redirect to home for 404 page after 5 seconds added a wildcard route for 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home"
 import SignUp from "./pages/signup"
 import SignIn from "./pages/signin";
+import NotFound from "./pages/notfound";
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   )
