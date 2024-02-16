b60915d05a7c1fb89cb80181d2266127296923e8
mishael-codes <enyimishael22@gmail.com>
2024-02-16 15:18:16 +0100

    bug fixes

--- src/pages/notfound.tsx
+++ src/pages/notfound.tsx
@@ -6,25 +6,29 @@ const NotFound = () => {
   const Redirect = () => {
     setTimeout(() => {
       window.location.href = "/";
-    }, 5000);
+    }, 4000);
 
     setInterval(() => {
       setCount(count - 1);
     }, 1000);
-    
+
     if (count === 1) {
       setSeconds("second");
     }
-    
+
     return (
       <div className="mt-5 w-[350px] text-center">
-        <p>Redirecting to Cuttr in {count} {seconds}...</p>
+        <p>
+          Redirecting to Cuttr in {count} {seconds}...
+        </p>
       </div>
     );
   };
   return (
     <div className="h-screen flex items-center justify-center flex-col text-text">
-      <h1 className="font-bold text-4xl"><span className="text-accent">Page</span> Not Found</h1>
+      <h1 className="font-bold text-4xl">
+        <span className="text-accent">Page</span> Not Found
+      </h1>
       <Redirect />
     </div>
   );
