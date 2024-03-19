ff624fa61ef8c6da3ea316b08f57ba17596fb10f
mishael-codes <enyimishael22@gmail.com>
2024-03-19 12:10:40 +0100

    updated footer

--- src/components/home/footer.tsx
+++ src/components/home/footer.tsx
@@ -1,8 +1,11 @@
+import ogCuttr from "../../assets/icons/og-Cuttr.png"
+
 const Footer: React.FC = () => {
   return (
     <div className="mt-24">
-      <div className="w-full h-20 bg-accent flex items-center justify-center text-background">
+      <div className="w-full h-20 bg-accent flex items-center justify-between px-2 text-background">
         <p className="font-semibold">&copy; 2021 Cuttr. All Rights Reserved</p>
+        <img src={ogCuttr} alt="Cuttr logo" className="justify-self-end" />
       </div>
     </div>
   );
