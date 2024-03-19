d9660fd51748c6c2ef27d4a0b2813aadcece0fbd
mishael-codes <enyimishael22@gmail.com>
2024-03-19 12:13:14 +0100

    minor changes

--- src/components/home/footer.tsx
+++ src/components/home/footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
     <div className="mt-24">
       <div className="w-full h-20 bg-accent flex items-center justify-between px-2 text-background">
         <p className="font-semibold">&copy; 2021 Cuttr. All Rights Reserved</p>
-        <img src={ogCuttr} alt="Cuttr logo" className="justify-self-end" />
+        <img src={ogCuttr} alt="Cuttr logo" className="justify-self-end rounded-lg" />
       </div>
     </div>
   );
